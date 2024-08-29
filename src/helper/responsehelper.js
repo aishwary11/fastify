e9457fbfecdb41ac6e0d21dc94afc2436e1e5a42
aishwary11 <aishwary46@gmail.com>
2024-08-30 00:47:54 +0530

    code optimize

--- src/helper/responsehelper.js
+++ src/helper/responsehelper.js
@@ -1,7 +1,3 @@
-const responseHelper = (reply, statusCode, message, data = null) => reply.status(statusCode).send({
-  status: statusCode < 400,
-  message: message,
-  data: data
-});
+const responseHelper = (reply, statusCode, message, data = null) => reply.status(statusCode).send({ status: statusCode < 400, message, data });
 
 export default responseHelper;
